f603c0a0e6c51b5d255d6a79740edae0bf66de75
KeaganRee <kyan.lee109@gmail.com>
2025-04-30 11:11:52 -0700

    Created Button Binding got string for new user id

--- app/src/main/java/com/example/cst_338_project_2_group_9/CreateAccountActivity.java
+++ app/src/main/java/com/example/cst_338_project_2_group_9/CreateAccountActivity.java
@@ -1,6 +1,40 @@
 package com.example.cst_338_project_2_group_9;
 
-public class CreateAccountActivity {
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cst_338_project_2_group_9.databinding.ActivityCreateaccountBinding;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    private ActivityCreateaccountBinding binding;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        binding = ActivityCreateaccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {createAccount();}
+        });
+    }
+
+    private void createAccount(){
+        String newUserId  = binding.createUserIDEditText.getText().toString();
+
+    }
+
+
+    public static Intent intentFactory(Context context) {
+        return new Intent(context, CreateAccountActivity.class);
+    }
+
 
 
 }
