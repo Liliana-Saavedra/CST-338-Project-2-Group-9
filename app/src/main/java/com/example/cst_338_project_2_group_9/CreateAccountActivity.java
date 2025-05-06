b55428478c4667300e5f2bfaf78b108e5d77d602
KeaganRee <kyan.lee109@gmail.com>
2025-05-06 14:48:05 -0700

    Added create account to manifest

--- app/src/main/java/com/example/cst_338_project_2_group_9/CreateAccountActivity.java
+++ app/src/main/java/com/example/cst_338_project_2_group_9/CreateAccountActivity.java
@@ -8,11 +8,8 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cst_338_project_2_group_9.databinding.ActivityCreateaccountBinding;
-import com.example.cst_338_project_2_group_9.databinding.ActivityLoginBinding;
 import com.example.cst_338_project_2_group_9.entities.User;
 import com.example.cst_338_project_2_group_9.typeConverters.AppDatabase;
-
-import java.util.List;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -26,14 +23,11 @@ public class CreateAccountActivity extends AppCompatActivity {
         binding = ActivityCreateaccountBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_createaccount);
 
-        db = AppDatabase.getDatabase(this);
-
         binding.createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 createAccount();
             }
-
         });
     }
 
@@ -43,12 +37,13 @@ public class CreateAccountActivity extends AppCompatActivity {
         String verifyPassword = binding.verifyPasswordEditText.getText().toString();
 
         new Thread(() -> {
-            List<User> users = db.userDAO().getAllUsers();
             if (newPassword.equals(verifyPassword)) {
                 User newUser = new User(newPassword, newUserId, false);
                 db.userDAO().insert(newUser);
+                startActivity(LoginActivity.intentFactory(getApplicationContext()));
             }
         }).start();
+
 
     }
 
