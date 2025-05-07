8cc112f69bec00e6e2134622e505fa18efa77121
KeaganRee <kyan.lee109@gmail.com>
2025-05-06 19:05:09 -0700

    Various small fixes
    Started Admin activity

--- app/src/main/java/com/example/cst_338_project_2_group_9/AdminLandingPage.java
+++ app/src/main/java/com/example/cst_338_project_2_group_9/AdminLandingPage.java
@@ -3,25 +3,75 @@ package com.example.cst_338_project_2_group_9;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.cst_338_project_2_group_9.databinding.ActivityAdminLandingPageBinding;
+import com.example.cst_338_project_2_group_9.entities.User;
+import com.example.cst_338_project_2_group_9.typeConverters.AppDatabase;
+
 
 public class AdminLandingPage extends AppCompatActivity {
+
+    private ActivityAdminLandingPageBinding binding;
+
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_landing_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityAdminLandingPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        db = AppDatabase.getDatabase(this);
+
+        binding.removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeUser();
+            }
         });
+
+        binding.homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(PlantMaintenanceActivity.intentFactory(getApplicationContext()));
+            }
+        });
+
+    }
+
+    private void removeUser(){
+        String userToRemove  = binding.removeUserEditText.getText().toString();
+        String adminUser = binding.adminUserName.getText().toString();
+        String adminPw = binding.adminPassword.getText().toString();
+
+        if(userToRemove.isEmpty()){
+            toastMaker("No user selected");
+            return;
+        }
+
+
+        if(adminUser.isEmpty() || adminPw.isEmpty()){
+            toastMaker("Invalid Login");
+            return;
+        }
+
+
+        new Thread(() -> {
+            User admin1 = db.userDAO().getUserByUsername(adminUser);
+            if(adminPw.equals(admin1.getPassword())) {
+                db.userDAO().deleteUser(db.userDAO().getUserByUsername(userToRemove));
+            }
+            else{
+                toastMaker("Invalid Login");
+            }
+        }).start();
+    }
+    private void toastMaker(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
     public static Intent AdminLandingPageIntentFactory(Context context) {
         return new Intent(context, AdminLandingPage.class);
